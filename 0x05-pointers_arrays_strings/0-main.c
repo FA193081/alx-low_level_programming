4cc6d7d5760fec3828f92ba4d94d4ce3cdddfc40
FA193081 <francagbor95@gmail.com>
2023-02-27 15:01:29 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,9 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-
-#endif
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+n = 402;
+printf("n=%d\n", n);
+reset_to_98(&n);
+printf("n=%d\n", n);
+return (0);
+}
