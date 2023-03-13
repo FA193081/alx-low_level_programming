797cc41d62bfc9534ea87de898fe399ba808f77b
FA193081 <francagbor95@gmail.com>
2023-03-13 01:02:15 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/*
- * print_alphabet_x10 - function that prints 10x alphabets
- *
- * Return: alwys 0
+/**
+ * print_alphabet_x10 - make alphabet  x10 times
+ * Return: void
  */
 
 void print_alphabet_x10(void)
+
 {
 char c;
 int i = 0;
